b00dc892a7b46cf16abdbc4d26a8dda66ab3819b
VasuSri <vasusrivastav1@gmail.com>
2024-11-20 06:13:43 +0530

    path resolve

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import path from 'path';
 const app = express();
 const port = 3000;
-
+// Middleware for static files and EJS templates
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
 
 app.get('/', (req, res) => {
