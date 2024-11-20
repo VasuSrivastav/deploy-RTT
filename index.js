3b4bf1d357964ceaf41776ebca1fb27f23cf1d15
VasuSri <vasusrivastav1@gmail.com>
2024-11-20 06:08:43 +0530

    view setup

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('hello.ejs');
 });
 
 app.listen(port, () => {
